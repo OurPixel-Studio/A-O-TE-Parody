3327a685089a0db3094362daa204bbf4ee1b9558
OurPixel-Studio <112803343+OurPixel-Studio@users.noreply.github.com>
2022-09-04 20:50:49 +0700

    Other scripts

---
+++ Script/TimeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowDownFactor = 0.05f;
+    public float slowDownLength = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+
+    public void DoSlowMotion()
+    {
+        Time.timeScale = slowDownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+}
---
+++ Script/UI3dm.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI3dm : MonoBehaviour
+{
+    public Text kondisi3dm;
+    public Slider gasTotal;
+    GameObject ManuverGear;
+    int kondisiManuver;
+    bool isiGas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ManuverGear = GameObject.FindGameObjectWithTag("3dm");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kondisiManuver = ManuverGear.GetComponent<ManuverGear>().kondisiManuver;
+        isiGas = ManuverGear.GetComponent<ManuverGear>().SedangIsiGas;
+
+        kondisi3dm.text = "Kondisi Manuver Gear = " + kondisiManuver;
+
+        if (isiGas == true)
+        {
+            kondisi3dm.text = "Sedang Isi Gas Bray";
+        }
+    }
+
+    public void Gas3dmMax(float jumlahGas)
+    {
+        gasTotal.maxValue = jumlahGas;
+        gasTotal.value = jumlahGas;
+    }
+
+    public void Gas3dm(float jumlahGas)
+    {
+        gasTotal.value = jumlahGas;
+    }
+}
